115f867a550856f31c52c4ab00bc18dac85df6b3
Márcia Soares <profamarcia2014@gmail.com>
2025-01-09 02:39:06 -0300

    Primeiro commit

---
+++ docs/credit_card_prefixes.md
@@ -0,0 +1,9 @@
+# Credit Card Prefixes
+
+| Bandeira            | Prefixos Numéricos       |
+|---------------------|--------------------------|
+| **Visa**            | 4                        |
+| **MasterCard**      | 51-55                    |
+| **American Express**| 34, 37                   |
+| **Discover**        | 6011, 622126-622925, 644-649, 65 |
+| **JCB**             | 3528-3589                |
---
+++ src/main.py
@@ -0,0 +1,25 @@
+def get_credit_card_flag(card_number):
+    card_number = str(card_number)
+    
+    # Prefixos das bandeiras
+    prefixes = {
+        "Visa": ["4"],
+        "MasterCard": [str(i) for i in range(51, 56)],
+        "American Express": ["34", "37"],
+        "Discover": ["6011"] + [str(i) for i in range(622126, 622926)] + [str(i) for i in range(644, 650)] + ["65"],
+        "JCB": [str(i) for i in range(3528, 3590)]
+    }
+    
+    for flag, prefix_list in prefixes.items():
+        for prefix in prefix_list:
+            if card_number.startswith(prefix):
+                return flag
+    
+    return "Bandeira desconhecida"
+
+# Exemplo de uso
+print(get_credit_card_flag("4111111111111111"))  # Visa
+print(get_credit_card_flag("5105105105105100"))  # MasterCard
+print(get_credit_card_flag("371449635398431"))   # American Express
+print(get_credit_card_flag("6011111111111117"))  # Discover
+print(get_credit_card_flag("3530111333300000"))  # JCB
